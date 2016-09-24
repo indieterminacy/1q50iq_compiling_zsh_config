8451a9e8ae19848f41a004d9cf9bc2012d92b512
indieterminacy <indieterminacy@nortonspeal.com>
2016-09-24 15:09:52 +0200

    Starting repo

---
+++ oqo_compiling.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/zsh
+
+#https://github.com/robbyrussell/oh-my-zsh/blob/master/plugins/gas/_gas
+####################
+# oq GNU_Assembler
+####################
+#https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/gas
